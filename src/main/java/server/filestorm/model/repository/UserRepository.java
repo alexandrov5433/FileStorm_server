9f85a5be333933120a4c15f970c37041c05b36bb
alexandrov5433 <alexandrov5433@gmail.com>
2025-05-27 13:23:26 +0200

    Query by email and by username and id added.

--- src/main/java/server/filestorm/model/repository/UserRepository.java
+++ src/main/java/server/filestorm/model/repository/UserRepository.java
@@ -13,6 +13,12 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE u.username = ?1")
     Optional<User> findUserByUsername(String username);
 
+    @Query("SELECT u FROM User u WHERE u.username = ?1 AND u.id = ?2")
+    Optional<User> findUserByUsernameAndId(String username, Integer id);
+    
     @Query("SELECT u.username, u.id FROM User u WHERE u.username ILIKE CONCAT('%', ?1, '%') ORDER BY u.username ASC")
     Optional<List<Object[]>> queryUsersByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> searchForUserWithThisEmail(String emial);
 }
