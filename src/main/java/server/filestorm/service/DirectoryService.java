b20d92af011a83cc93108ab2142cff3a21dace38
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-10 22:47:37 +0200

    Collection services added.

--- src/main/java/server/filestorm/service/DirectoryService.java
+++ src/main/java/server/filestorm/service/DirectoryService.java
@@ -1,10 +1,14 @@
 package server.filestorm.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import server.filestorm.exception.StorageException;
+import server.filestorm.model.entity.Chunk;
 import server.filestorm.model.entity.Directory;
 import server.filestorm.model.entity.User;
 import server.filestorm.model.repository.DirectoryRepository;
@@ -50,6 +54,35 @@ public class DirectoryService {
     public Directory findDirectoryForUserById(Long directoryId, User owner) {
         return directoryRepository.findDirectoryForUserById(directoryId, owner)
             .orElseThrow(() -> new StorageException("Directory could not be found."));
+    }
+
+    /**
+     * Collects all chunks from the given directory, it´s subdirectories and all other subdirectories down the directory tree.
+     * @param dir The directory from which the chunk collection must start.
+     * @return All collected chunks.
+     */
+    public ArrayList<Chunk> extractChunksFromDirAndSubDirs(Directory dir) {
+        ArrayList<Chunk> chunks = new ArrayList<Chunk>(dir.getChunks());
+        List<Directory> subdirectories = dir.getSubdirectories();
+        for (Directory subdir : subdirectories) {
+            ArrayList<Chunk> subChunks = extractChunksFromDirAndSubDirs(subdir);
+            chunks.addAll(subChunks);
+        }
+        return chunks;
+    }
+
+    /**
+     * Collects all subdirectories form the given directory and from all directories found down the directory tree. The initially given (first) directory IS NOT included in the final result.
+     * @param dir The initial (first) directory, from which the collection must start. 
+     * @return All collected directories.
+     */
+    public ArrayList<Directory> extractDirectoriesFromDirAndSubDirs(Directory dir) {
+        ArrayList<Directory> subdirectories = new ArrayList<Directory>(dir.getSubdirectories());
+        for (Directory subdir : subdirectories) {
+            ArrayList<Directory> deeperSubdirs = extractDirectoriesFromDirAndSubDirs(subdir);
+            subdirectories.addAll(deeperSubdirs);
+        }
+        return subdirectories;
     }
 
 }
