a016a428ab062531476fe5b06a979cdf7bd31fd3
alexandrov5433 <alexandrov5433@gmail.com>
2025-06-11 11:33:09 +0200

    Route guard updated.

--- src/main/java/server/filestorm/filter/RouteGuard.java
+++ src/main/java/server/filestorm/filter/RouteGuard.java
@@ -21,34 +21,27 @@ import server.filestorm.util.CustomHttpServletRequestWrapper;
 @Order(2)
 @Component
 public class RouteGuard implements Filter {
-    
+
     @Override
     public void doFilter(
-        ServletRequest req,
-        ServletResponse res,
-        FilterChain filterChain
-    )throws ServletException, IOException {
+            ServletRequest req,
+            ServletResponse res,
+            FilterChain filterChain) throws ServletException, IOException {
         CustomHttpServletRequestWrapper customReq = (CustomHttpServletRequestWrapper) req;
         HttpServletRequest httpReq = (HttpServletRequest) req;
         HttpServletResponse httpRes = (HttpServletResponse) res;
         String httpMethod = httpReq.getMethod().toUpperCase();
         String reqPath = httpReq.getRequestURI();
 
-
         HashMap<String, String> endpointMap = new HashMap<>();
         // Client - not protected
         // Authentication - not protected
         // FileSystem
-        endpointMap.put("/api/file", "^GET|POST|DELETE$");
-        endpointMap.put("/api/file/name/.+", "^PATCH$");
-        endpointMap.put("/api/directory", "^GET|POST|DELETE$");
+        endpointMap.put("/api/file.*", "^GET|POST|DELETE|PATCH$");
+        endpointMap.put("/api/directory.*", "^GET|POST|DELETE$");
         // FileSharing
-        endpointMap.put("/api/file/share_option/.+/.+", "^PATCH$");
+        endpointMap.put("/api/file-sharing.*", "^GET|POST|PATCH$");
         endpointMap.put("/api/users", "^GET$");
-        endpointMap.put("/api/file/share_with_user/.+/.+", "^POST$");
-        endpointMap.put("/api/file/shared_with_me", "^GET$");
-        endpointMap.put("/api/file/me_sharing", "^GET$");
-        endpointMap.put("/api/file/share_with/.+", "^GET$");
         // Favorite
         endpointMap.put("/api/favorite.*", "^GET|POST|DELETE$");
 
@@ -70,12 +63,14 @@ public class RouteGuard implements Filter {
         if (isTargetPathSecure) {
             CustomSession session = customReq.getCustomSession();
             if (session == null ||
-            session.getClaims() == null ||
-            session.getUserId() == null ||
-            session.getUsername() == null) {
+                session.getClaims() == null ||
+                session.getUserId() == null ||
+                session.getUsername() == null) {
+
                 httpRes.setStatus(400);
                 httpRes.setHeader("Content-Type", "application/json");
                 httpRes.getWriter().write("{\"message\": \"Not authenticated.\", \"payload\": null}");
+
                 return;
             }
         }
