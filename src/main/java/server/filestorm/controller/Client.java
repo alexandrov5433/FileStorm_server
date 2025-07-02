70b61db42f855f5bfe889390dc7e97cae825f5fe
alexandrov5433 <alexandrov5433@gmail.com>
2025-07-02 17:39:18 +0200

    Execution of processes on a separate thread.

--- src/main/java/server/filestorm/controller/Client.java
+++ src/main/java/server/filestorm/controller/Client.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import server.filestorm.service.ClientService;
+import server.filestorm.thread.ThreadExecutorService;
 
 @Controller
 public class Client {
@@ -17,26 +18,34 @@ public class Client {
     @Autowired
     private ClientService clientService;
 
+    @Autowired
+    private ThreadExecutorService threadExecutorService;
+
     @GetMapping("/")
     public DeferredResult<ResponseEntity<?>> getClient() throws IOException {
         DeferredResult<ResponseEntity<?>> res = new DeferredResult<ResponseEntity<?>>();
-        try {
-            Resource clientHtml = clientService.loadClientHtmlAsResource();
-            if (clientHtml == null) {
+
+        Runnable process = () -> {
+            try {
+                Resource clientHtml = clientService.loadClientHtmlAsResource();
+                if (clientHtml == null) {
+                    res.setResult(ResponseEntity.notFound().build());
+                    return;
+                }
+                Long contentLength = clientHtml.contentLength();
+                res.setResult(ResponseEntity
+                        .ok()
+                        .header("Content-Type", "text/html")
+                        .header("Content-Length", Long.toString(contentLength))
+                        .body(clientHtml));
+            } catch (Exception e) {
                 res.setResult(ResponseEntity.notFound().build());
-                return res;
             }
-            Long contentLength = clientHtml.contentLength();
-            res.setResult(ResponseEntity
-                    .ok()
-                    .header("Content-Type", "text/html")
-                    .header("Content-Length", Long.toString(contentLength))
-                    .body(clientHtml));
-            return res;
-        } catch (Exception e) {
-            res.setResult(ResponseEntity.notFound().build());
-            return res;
-        }
+        };
+
+        threadExecutorService.execute(process);
+        
+        return res;
     }
 
 }
